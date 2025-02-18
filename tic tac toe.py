e00d8bc341acc60598256626ece75f98351dfb9b
Raynell Rajeev <108294211+raynellrajeev@users.noreply.github.com>
2025-02-18 12:15:02 +0530

    modify

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,115 +1,103 @@
-
 import random
-boardelements = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-winner = None
-game = True
-mode = int(input("1.play with a friend\n2.play with computer\n:"))
-player = "x"
 
-def clear(board_elements):
-    global boardelements
-    boardelements = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    #return boardelements
+class TicTacToe:
+    def __init__(self):
+        self.board = [" " for _ in range(9)]
+        self.winner = None
+        self.current_player = "X"
+        self.game_running = True
+        
+    def display_board(self):
+        for i in range(0, 9, 3):
+            print(f" {self.board[i]} | {self.board[i+1]} | {self.board[i+2]} ")
+            if i < 6:
+                print("-----------")
+    
+    def player_move(self):
+        while True:
+            try:
+                position = int(input(f"Player {self.current_player}, enter position (1-9): ")) - 1
+                if 0 <= position <= 8 and self.board[position] == " ":
+                    self.board[position] = self.current_player
+                    break
+                else:
+                    print("Invalid position! Try again.")
+            except ValueError:
+                print("Please enter a number between 1 and 9!")
+    
+    def computer_move(self):
+        empty_positions = [i for i, spot in enumerate(self.board) if spot == " "]
+        if empty_positions:
+            position = random.choice(empty_positions)
+            self.board[position] = self.current_player
+    
+    def check_winner(self):
+        # Check winning combinations
+        win_combinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertical
+            [0, 4, 8], [2, 4, 6]              # Diagonal
+        ]
+        
+        for combo in win_combinations:
+            if (self.board[combo[0]] == self.board[combo[1]] == 
+                self.board[combo[2]] != " "):
+                self.winner = self.board[combo[0]]
+                return True
+        return False
+    
+    def check_tie(self):
+        return " " not in self.board
+    
+    def switch_player(self):
+        self.current_player = "O" if self.current_player == "X" else "X"
+    
+    def play_again(self):
+        while True:
+            choice = input("Play again? (y/n): ").lower()
+            if choice in ['y', 'n']:
+                return choice == 'y'
+            print("Please enter 'y' or 'n'")
+    
+    def play_game(self):
+        print("\nWelcome to Tic Tac Toe!")
+        while True:
+            try:
+                mode = int(input("1. Play with a friend\n2. Play with computer\nSelect mode: "))
+                if mode in [1, 2]:
+                    break
+                print("Please enter 1 or 2!")
+            except ValueError:
+                print("Please enter a valid number!")
+        
+        while self.game_running:
+            self.display_board()
+            
+            if mode == 1 or (mode == 2 and self.current_player == "X"):
+                self.player_move()
+            else:
+                self.computer_move()
+            
+            if self.check_winner():
+                self.display_board()
+                print(f"\nPlayer {self.winner} wins!")
+                if not self.play_again():
+                    break
+                self.__init__()  # Reset the game
+                continue
+            
+            if self.check_tie():
+                self.display_board()
+                print("\nIt's a tie!")
+                if not self.play_again():
+                    break
+                self.__init__()  # Reset the game
+                continue
+            
+            self.switch_player()
+        
+        print("Thanks for playing!")
 
-def board(boardelements):
-    print(boardelements[0], " | ", boardelements[1], " | ", boardelements[2])
-    print("--- ----- ---")
-    print(boardelements[3], " | ", boardelements[4], " | ", boardelements[5])
-    print("--- ----- ---")
-    print(boardelements[6], " | ", boardelements[7], " | ", boardelements[8])
-
-def playerinput(boardelements):
-    inp = int(input("enter a number between 1 and 9 :"))
-    if inp >= 1 and inp <= 9 and boardelements[inp-1] == " ":
-        boardelements[inp-1] = player
-    else:
-        print("error")
-
-def hor(boardelements):
-    global winner
-    if boardelements[0] == boardelements[1] == boardelements[2] and boardelements[1] != " ":
-        winner = boardelements[0]
-        return True
-    elif boardelements[3] == boardelements[4] == boardelements[5] and boardelements[3] != " ":
-        winner = boardelements[3]
-        return True
-    elif boardelements[6] == boardelements[7] == boardelements[8] and boardelements[8] != " ":
-        winner = boardelements[6]
-        return True
-
-def ver(boardelements):
-    global winner
-    if boardelements[0] == boardelements[3] == boardelements[6] and boardelements[0] != " ":
-        winner = boardelements[0]
-        return True
-    elif boardelements[1] == boardelements[4] == boardelements[7] and boardelements[1] != " ":
-        winner = boardelements[1]
-        return True
-    elif boardelements[2] == boardelements[5] == boardelements[8] and boardelements[2] != " ":
-        winner = boardelements[2]
-        return True
-
-def dia(boardelements):
-    global winner
-    if boardelements[0] == boardelements[4] == boardelements[8] and boardelements[0] != " ":
-        winner = boardelements[0]
-        return True
-    elif boardelements[2] == boardelements[4] == boardelements[6] and boardelements[2] != " ":
-        winner = boardelements[2]
-        return True
-
-def tie(boardelements):
-    global game
-    if " " not in boardelements:
-        board(boardelements)
-        print("--------TIE--------")
-        board(boardelements)
-        choice()
-        clear(boardelements)
-
-def win():
-    global game
-    if hor(boardelements) or ver(boardelements) or dia(boardelements):
-        print(winner, " WINS! ")
-        board(boardelements)
-        choice()
-        clear(boardelements)
-
-def switch():
-    global player
-    if player == "x":
-        player = "o"
-    else:
-        player = "x"
-
-def comp(boardelements):
-    while player == "o":
-        pos = random.randint(0,8)
-        if boardelements[pos] == " ":
-            boardelements[pos] = "o"
-            switch()
-
-def choice():
-    global game
-    choice = int(input("1.Play again\n2.quit\n:"))
-    if choice == 2:
-        print("Thank you for playing")
-        game = False
-    else:
-        clear(boardelements)
-        game = True
-
-while game:
-    if mode == 1:
-        board(boardelements)
-        playerinput(boardelements)
-        switch()
-        win()
-        tie(boardelements)
-    elif mode == 2:
-        board(boardelements)
-        playerinput(boardelements)
-        switch()
-        comp(boardelements)
-        win()
-        tie(boardelements)
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.play_game()
